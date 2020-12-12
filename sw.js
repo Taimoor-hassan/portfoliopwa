689b6332e78b725167b7ef8796c6c2547ae2d010
Taimoor hassan <king.taimoor98.th@gmail.com>
2020-12-12 18:57:54 +0500

    1

---
+++ sw.js
@@ -0,0 +1,34 @@
+self.addEventListener('install', function (event) {
+  console.log('SW Installed');
+  event.waitUntil(
+    caches.open('static')
+      .then(function (cache) {
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/app.js',
+          '/images/icon.png',
+          '/images/Me.jpg',
+          '/blue.css',
+          '/default.css'
+        ]);
+      })
+  );
+});
+
+self.addEventListener('activate', function () {
+  console.log('SW Activated');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        if (res) {
+          return res;
+        } else {
+          return fetch(event.request);
+        }
+      })
+  );
+});
